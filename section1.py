f6beddb26f3136308886be0d5f1410a308dff98a
achala0307 <hiroko.ski.math.dog@gmail.com>
2021-03-07 21:16:39 +0900

    createnewfile

--- section1.py
+++ section1.py
@@ -1,1 +1,7 @@
-print('helloworld')
+import requests
+import io
+import pandas as pd
+
+url = 'http://www.minethatdata.com/Kevin_Hillstrom_MineThatData_E-MailAnalytics_DataMiningChallenge_2008.03.20.csv'
+res = requests.get(url).content
+df = pd.read_csv(io.StringIO(res.decode('utf-8')), header=0, index_col=0)
